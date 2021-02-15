c74cfd04491ae3f2275ed0b01c7ca16e1e244f81
Kyle Johns <kjohns1337@gmail.com>
2021-02-14 21:41:10 -0500

    simple byte extension

---
+++ Extensions/ByteExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardanoSharp.DbSync.EFCore.Extensions
+{
+    public static class ByteExtensions
+    {
+        public static string GetHex(this byte[] ba)
+        {
+            return BitConverter.ToString(ba).Replace("-", "").ToLower();
+        }
+    }
+}
